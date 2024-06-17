943b9f91a08d974d4f1893085e0d1f6d3ca2b085
Paulo M.Pereira da Silva Ramos <paulo.1209@terra.com.br>
2024-06-17 07:16:24 -0300

    guardina

---
+++ src/app/auth/auth-guard.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+
+import decode from 'jwt-decode'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService {
+
+  constructor(private jwtHelper:JwtHelperService, private router:Router) { }
+
+  canActivate(): boolean {
+    const token = localStorage.getItem('PegarToken');
+
+    if(token){
+      return true;
+    }
+    this.router.navigate(['login'])
+  }
+
+  VerificaAdmin():boolean{
+    const token = localStorage.getItem('PegarToken');
+    const usuarioToken = decode(token);
+
+    if(usuarioToken.role === 'Adm'){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
